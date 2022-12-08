549ba940b3dfae85906daef5717a038f44951dff
EmberDecode <mr.nik.patidar69@gmail.com>
2022-12-08 15:52:13 +0530

    core

---
+++ CoreJava/src/javabasics/Factorial.java
@@ -0,0 +1,11 @@
+package javabasics;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		  
+		 for (int i = 1; i <= 10; i++) {
+			 int a = i*3;
+		 System.out.println(a);
+
+		 }}}
